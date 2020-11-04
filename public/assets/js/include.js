c0c83f7aa0f50dfdfbcab5c68dcc0d33faa3b553
Devendra Jadhav <deve9594@gmail.com>
2020-11-04 23:16:05 +0530

    charity website deploy to firebase

---
+++ public/assets/js/include.js
@@ -0,0 +1,15 @@
+ // $(function(){
+ //      $("#header").load("header.html"); 
+ //    });
+
+ 
+
+$(document).ready(function(){
+ 	alert("include working");
+ 	// $(function(){
+  //     $("footer").load("footer.html"); 
+  //   });
+  jQuery(document).ready(function(){
+       jQuery("footer").load("footer.html");
+   });
+});
